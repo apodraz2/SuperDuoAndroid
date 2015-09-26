ea14878011ecf192b42933d8ac8c383ed2de2a08
Adam Podraza <apodra86@gmail.com>
2015-09-26 14:15:17 -0600

    first attempt at dynamic data in simple widget

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootballScoresWidget.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootballScoresWidget.java
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 
@@ -12,6 +14,7 @@ import android.widget.RemoteViews;
  * Implementation of App Widget functionality.
  */
 public class FootballScoresWidget extends AppWidgetProvider {
+    private static final String LOG_TAG = "football_scores_widget";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -35,19 +38,14 @@ public class FootballScoresWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        Log.d(LOG_TAG, "updateAppWidget");
+        context.startService(new Intent(context, FootballWidgetIntentService.class));
+    }
 
-        CharSequence widgetText = "Manchester City FC";
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.football_scores_widget);
-        views.setImageViewResource(R.id.widget_icon, R.drawable.manchester_city);
-        views.setTextViewText(R.id.widget_favorite_team_name, widgetText);
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
 
-        Intent launchIntent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-        views.setOnClickPendingIntent(R.id.simple_widget, pendingIntent);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
 
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootballWidgetIntentService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootballWidgetIntentService.java
@@ -1,16 +1,20 @@
 package barqsoft.footballscores;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
 
 /**
  * Created by adampodraza on 9/26/15.
  */
 public class FootballWidgetIntentService extends IntentService {
+    private final String LOG_TAG = getClass().getSimpleName();
 
     private static final String[] FOOTBALL_COLUMNS = {
             DatabaseContract.scores_table.HOME_COL,
@@ -21,6 +25,10 @@ public class FootballWidgetIntentService extends IntentService {
     };
 
     private static final int HOME_COL = 3;
+    private static final int AWAY_COL = 4;
+    private static final int HOME_GOALS_COL = 6;
+    private static final int AWAY_GOALS_COL = 7;
+
 
 
     public FootballWidgetIntentService() {
@@ -29,6 +37,7 @@ public class FootballWidgetIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d(LOG_TAG, "onHandleIntent");
 
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
 
@@ -52,7 +61,30 @@ public class FootballWidgetIntentService extends IntentService {
              return;
          }
 
-        String homeTeam;
+        String homeTeam = data.getString(HOME_COL);
+        String awayTeam = data.getString(AWAY_COL);
+        int homeGoals = data.getInt(HOME_GOALS_COL);
+        int awayGoals = data.getInt(AWAY_GOALS_COL);
+
+        data.close();
+
+        for (int appId : appWidgetIds) {
+            int layoutId = R.layout.football_scores_widget;
+
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+
+            views.setImageViewResource(R.id.widget_icon, Utilies.getTeamCrestByTeamName(homeTeam));
+            views.setTextViewText(R.id.widget_home_team_name, homeTeam);
+            views.setTextViewText(R.id.widget_home_team_score, Integer.toString(homeGoals));
+
+            // Create an Intent to launch MainActivity
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.simple_widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appId, views);
+        }
 
     }
 }
